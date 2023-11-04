f0abcfd9911cd71f49d2254a31257e4c955b0a38
Mikhail Dunaev <mih.dunaev@gmail.com>
2023-11-04 14:05:51 +0100

    Add player character animation

---
+++ Assets/Scripts/Game/Item.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Game.UI
+{
+    [CreateAssetMenu(menuName = "Create Item", fileName = "Item", order = 0)]
+    public class Item : ScriptableObject
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+
+        private const string PrefabsPath = "Prefabs/Items/";
+        
+        public static Item GetItem(string key)
+        {
+            var item = Resources.Load<Item>(PrefabsPath + key);
+            if (item == null)
+            {
+                Debug.LogError($"Item {key} not found.");
+                return null;
+            }
+            
+            return item;
+        }
+    }
+}
--- Assets/Scripts/Game/UI/InventoryUI.cs
+++ Assets/Scripts/Game/UI/InventoryUI.cs
@@ -1,7 +1,24 @@
-﻿namespace Game.UI
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.UI
 {
-    public class InventoryUI
+    public class InventoryUI : MonoBehaviour
     {
+        private readonly List<Item> _items = new();
+
+        public void AddItem(string key)
+        {
+            var item = Item.GetItem(key);
+            if (item != null)
+            {
+                _items.Add(item);
+            }
+        }
         
+        public void RemoveItem(string key)
+        {
+            _items.RemoveAll(item => item.Key == key);
+        }
     }
 }
