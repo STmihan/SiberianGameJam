0a0a67bd649ed9461cd2b15ebc708fb65b42026a
Mikhail Dunaev <mih.dunaev@gmail.com>
2023-11-06 16:39:32 +0100

    Completing stuff 4

--- Assets/Scripts/Game/Services/MusicService.cs
+++ Assets/Scripts/Game/Services/MusicService.cs
@@ -5,5 +5,11 @@ namespace Game.Services
     public class MusicService : MonoBehaviour
     {
         [SerializeField] private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            DontDestroyOnLoad(gameObject);
+        }
     }
 }
